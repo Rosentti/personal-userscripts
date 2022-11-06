cb0d830f8ea8cb9a3218a47ae53b74a790efae17
20PercentRendered <32398752+20PercentRendered@users.noreply.github.com>
2022-11-06 17:26:12 +0200

    Update and rename Wilma auto-click login button.user.js to autoclicklogin.user.js

--- autoclicklogin.user.js
+++ autoclicklogin.user.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name         Wilma auto-click login button
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  many do
 // @author       You
+// @downloadURL  https://github.com/20PercentRendered/personal-userscripts/raw/master/autoclicklogin.user.js
+// @updateURL    https://github.com/20PercentRendered/personal-userscripts/raw/master/autoclicklogin.user.js
 // @match        https://riveria.inschool.fi/
 // @match        https://riveria.inschool.fi/login?sessionexpired
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
